28d308c44f6682ebf08475f63d997390e46129d6
jerry <aafulin@gmail.com>
2022-07-17 15:41:12 +0800

    lazy - step2 (delete unused component)

--- src/app/utilities/utilities-routing.module.ts
+++ src/app/utilities/utilities-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { UtilitiesComponent } from './utilities.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{ path: '', component: UtilitiesComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UtilitiesRoutingModule { }
+export class UtilitiesRoutingModule {
+}
--- src/app/utilities/utilities.module.ts
+++ src/app/utilities/utilities.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UtilitiesRoutingModule } from './utilities-routing.module';
-import { UtilitiesComponent } from './utilities.component';
 
 
 @NgModule({
   declarations: [
-    UtilitiesComponent
   ],
   imports: [
     CommonModule,
