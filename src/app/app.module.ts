daec5d53fa76ec5b80ad1ae19eb58e779464e17e
jerry <aafulin@gmail.com>
2022-07-17 11:11:52 +0800

    示範路由連結、預設路由、轉向路由、萬用路由、子路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
     Page2Component,
-    DashboardComponent
+    DashboardComponent,
+    NotFoundComponent,
+    ColorsComponent
   ],
   imports: [
     BrowserModule,
