f30aa25d86217887883ea767f964af40c62d5869
jerry <aafulin@gmail.com>
2022-07-17 15:40:05 +0800

    lazy - step1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
       },
     ]
   },
+  { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
   // {path: '**', component: NotFoundComponent},
 ];
 
