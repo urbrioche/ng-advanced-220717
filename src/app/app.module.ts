17f2c5fa22b8329665d4c0c0056a51e38ec294b3
jerry <aafulin@gmail.com>
2022-07-17 10:15:42 +0800

    設定基本路由規則+啟用 HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
