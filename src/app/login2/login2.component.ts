77ef764abf7218c008524a91015d645afaca2fdf
jerry <aafulin@gmail.com>
2022-07-24 15:50:09 +0800

    實作 FormArray 搭配 FormGroup

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -26,7 +26,17 @@ export class Login2Component implements OnInit {
     password: this.fb.control('', {
       validators: [Validators.required]
     }),
-    isRememberMe: this.fb.control(true, {})
+    isRememberMe: this.fb.control(true, {}),
+    profiles: this.fb.array([
+      this.fb.group({
+        city: this.fb.control('Taipei', {validators: [Validators.required]}),
+        tel: this.fb.control('0988-888888', {validators: [Validators.required]})
+      }),
+      this.fb.group({
+        city: this.fb.control('Taichung', {validators: [Validators.required]}),
+        tel: this.fb.control('0944-444444', {validators: [Validators.required]})
+      }),
+    ])
   })
 
   constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) {
