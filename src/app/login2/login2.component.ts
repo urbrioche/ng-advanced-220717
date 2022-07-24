13d524d480f6a089ae557d5cc8d9ba1ed92aba5a
jerry <aafulin@gmail.com>
2022-07-24 20:35:02 +0800

    示範動態欄位的開發方法

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -27,14 +27,8 @@ export class Login2Component implements OnInit {
     }),
     isRememberMe: this.fb.control(true, {}),
     profiles: this.fb.array([
-      this.fb.group({
-        city: this.fb.control('Taipei', {validators: [Validators.required]}),
-        tel: this.fb.control('0988-888888', {validators: [Validators.required]}),
-      }),
-      this.fb.group({
-        city: this.fb.control('Taichung', {validators: [Validators.required]}),
-        tel: this.fb.control('0944-444444', {validators: [Validators.required]}),
-      })
+      this.makeProfile('Taipei', '0988-888888'),
+      this.makeProfile('Taichung', '0944-444444'),
     ])
   })
 
@@ -69,6 +63,17 @@ export class Login2Component implements OnInit {
 
   resetForm() {
     this.form.reset(this.data);
+  }
+
+  addProfile() {
+    this.form.controls.profiles.push(this.makeProfile('', ''))
+  }
+
+  makeProfile(city: string, tel: string) {
+    return this.fb.group({
+      city: this.fb.control(city, {validators: [Validators.required]}),
+      tel: this.fb.control(tel, {validators: [Validators.required]})
+    });
   }
 
   // doLogin(form: NgForm) {
